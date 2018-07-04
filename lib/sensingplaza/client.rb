8532054f310b2fc2a0d88e085886c1f7364ef0e1
SOMEI Yoshino <yoshi@MacBookAir-2.local>
2018-07-04 10:17:30 +0900

    add push_data

--- lib/sensingplaza/client.rb
+++ lib/sensingplaza/client.rb
@@ -22,7 +22,7 @@ module Sensingplaza
       @webreq.url = val
     end
     
-    # sensorkey - String  ex) "12abe9de" or ["12abe9de", "433bedd2", ...]
+    # sensorkey - String(s)  ex) "12abe9de" or ["12abe9de", "433bedd2", ...]
     # datetime - String  ex) "2018-07-04 12:00:00"
     def get_data(sensorkey, datetime)
       return nil if @mailaddress.nil?
@@ -32,10 +32,14 @@ module Sensingplaza
 
       skeys = Array.new
       skeys.push(sensorkey) if sensorkey.instance_of?(String)
-      skeys = sensorkey if sensorkey.instance_of?(Array)
+      if sensorkey.instance_of?(Array)
+        if (sensorkey.collect{|c| c.instance_of?(String)} - [true]).empty?
+          skeys = sensorkey
+        end
+      end
       return nil if skeys.empty?
       
-      values = Hash.new
+      result = Hash.new
       endpoint = "/api/download"
       req = { "mailaddress" => @mailaddress,
               "datetime" => datetime,
@@ -44,11 +48,56 @@ module Sensingplaza
       jsonstr = @webreq.post(req, endpoint)
       response = JSON.parse(jsonstr)
       response["data"].each do |skey, val|
-        values[skey] = val
+        result[skey] = val
       end
-      return values
+      return result
     end
-        
+
+    # sensorkey - String(s)  ex) "" or ["", "", ...]
+    # datetime - String  ex) "2018-07-04 12:34:00"
+    # data - value(s)  ex) 12.45 or [12.45, "on", ...] 
+    def push_data(sensorkey, datetime, data)
+      return nil if @mailaddress.nil?
+      unless datetime.match(/^\d{4}\-\d{2}\-\d{2} \d{2}\:\d{2}:\d{2}$/)
+        return nil
+      end
+
+      skeys = Array.new
+      skeys.push(sensorkey) if sensorkey.instance_of?(String)
+      if sensorkey.instance_of?(Array)
+        if (sensorkey.collect{|c| c.instance_of?(String)} - [true]).empty?
+          skeys = sensorkey
+        end
+      end
+      return nil if skeys.empty?
+
+      vals = Array.new
+      if data.instance_of?(Array)
+        vals = data
+      else
+        vals.push(data)
+      end
+      return nil if vals.empty?
+
+      sensvals = Hash.new
+      skeys.each_with_index do |skey, i|
+        sensvals[skey] = vals[i]
+      end
+
+      result = Hash.new
+      endpoint = "/api/upload"
+      req = { "mailaddress" => @mailaddress,
+              "datetime" => datetime,
+              "data" => sensvals
+            }
+      jsonstr = @webreq.post(req, endpoint)
+      response = JSON.parse(jsonstr)
+      response["data"].each do |skey, val|
+        result[skey] = val
+      end
+      return result
+    end
+    
   end
 
 
